cabe1d976d1f4fecc702721a832971d68a7479e7
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-11-20 14:56:53 +0000

    Done.

--- reducer.js
+++ reducer.js
@@ -1,28 +1,21 @@
-let state = {count: 0}
+let state = {count: 0};
+ 
 function changeState(state, action){
-  switch (action.type) {
-    case 'INCREASE_COUNT':
-      return {count: state.count + 1}
-    default:
-      return state;
+    switch (action.type) {
+      case 'INCREASE_COUNT':
+        return {count: state.count + 1}
+      default:
+        return state;
+    }
   }
+ 
+function render(){
+    document.body.textContent = state.count
 }
-<<<<<<< HEAD
-render = () => (document.body.textContent = state.count)
-=======
-
-render = () => (document.body.textContent = state.count)
-
->>>>>>> 46a14cf5ce68e869d3ceae526451c5490534bd42
+ 
 function dispatch(action){
-  state = changeState(state,action)
+  state = changeState(state, action)
   render()
 }
-<<<<<<< HEAD
+ 
 render()
-=======
-
-render()
-
-
->>>>>>> 46a14cf5ce68e869d3ceae526451c5490534bd42
